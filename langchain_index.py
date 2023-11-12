99c7abd91dbd2d2ef6d5cbf721c9d488d8b60dd6
pontaryagin <ponta1496@gmail.com>
2023-11-13 01:58:29 +0900

    init

--- langchain_index.py
+++ langchain_index.py
@@ -1,15 +1,52 @@
-﻿from langchain.document_loaders import PDFMinerLoader
+#!/usr/bin/env python
 
-loader = PDFMinerLoader("AR02ALA_UM01_21J.pdf")
-documents = loader.load()
-
+# usage ./langchain_index.py "Markov-Functional_Interest_Rate_Models.pdf"
+import pysqlite_load
+from pathlib import Path
+from langchain.document_loaders import PDFMinerLoader
+import sys
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=0)
-texts = text_splitter.split_documents(documents)
-
+from pypdf import PdfReader
+from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-embeddings = OpenAIEmbeddings()
+from langchain.text_splitter import CharacterTextSplitter
+from itertools import chain
+import re
 
-from langchain.vectorstores import Chroma
-db = Chroma.from_documents(texts, embeddings, persist_directory="./storage")
-db.persist()
+def fix_eol(text:str):
+    return text.replace("-\n", "")
+
+REG_PAGE_PAT = re.compile(r"\{[\d ]+\{$")
+def fix_page(text:str):
+    return re.sub(REG_PAGE_PAT, "", text)
+
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0, separator=".\n")
+
+def get_pdf(path_to_pdf: str):
+    loader = PDFMinerLoader(path_to_pdf)
+    documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    print(len(texts))
+    embeddings = OpenAIEmbeddings()
+    db: Chroma = Chroma.from_documents(texts, embeddings, persist_directory="./storage")
+    return db
+
+def get_texts(path_to_pdf: str):
+    loader = PdfReader(path_to_pdf)
+    texts = []
+    for page in loader.pages:
+        page.extract_text()
+        raw_text = page.extract_text()
+        raw_text = fix_eol(raw_text)
+        raw_text = fix_page(raw_text)
+        texts.append(raw_text)
+    chunked_texts = text_splitter.split_text("\n".join(texts))
+    embeddings = OpenAIEmbeddings()
+    db: Chroma = Chroma.from_texts(chunked_texts, embeddings, persist_directory="./storage")
+    return db
+
+path_to_pdf = sys.argv[1] if len(sys.argv) > 1 else "Markov-Functional_Interest_Rate_Models.pdf"
+
+# texts = get_pdf(path_to_pdf)
+db = get_texts(path_to_pdf)
--- langchain_query.py
+++ langchain_query.py
@@ -1,23 +1,25 @@
-﻿from langchain.embeddings import OpenAIEmbeddings
-embeddings = OpenAIEmbeddings()
+#!/usr/bin/env python3
+import pysqlite_load
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
 
-from langchain.vectorstores import Chroma
+embeddings = OpenAIEmbeddings()
 db = Chroma(persist_directory="./storage", embedding_function=embeddings)
 retriever = db.as_retriever()
 
-from langchain.llms import OpenAI
-llm = OpenAI(model_name="text-davinci-003", temperature=0, max_tokens=500)
+model_name = "gpt-3.5-turbo"
+# model_name = "gpt-4"
+llm = ChatOpenAI(model=model_name, temperature=0, max_tokens=500)
 
-from langchain.chains import RetrievalQA
 qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
 
-import sys
-args = sys.argv
-if len(args) >= 2:
-	query = args[1]
-else:
-	query = "ailia SDKが対応しているOSを教えてください。"
-answer = qa.run(query)
+question = "please summarize the document"
 
-print("Q:", query)
-print("A:", answer)
+while True:
+	print("Q:" + (question or ""))
+	query = question or input()
+	question = None
+	answer = qa.run(query)
+	print("A:", answer)
