12fb5aaeae39ff571c83812be9cfaabbc9c6b68a
pontaryagin <ponta1496@gmail.com>
2023-11-24 00:18:26 +0900

    print retrieved documents

--- langchain_query.py
+++ langchain_query.py
@@ -4,10 +4,14 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
+from langchain.schema.document import Document
 
 embeddings = OpenAIEmbeddings()
 db = Chroma(persist_directory="./storage", embedding_function=embeddings)
-retriever = db.as_retriever()
+retriever = db.as_retriever(
+	search_type="mmr",
+	search_kwargs={'k': 6, 'lambda_mult': 0.25}
+	)
 
 model_name = "gpt-3.5-turbo"
 # model_name = "gpt-4"
@@ -25,6 +29,8 @@ while True:
 	# answer = qa.run(query)
 	res = qa({"query": query})
 	res["source_documents"][0]
-	pages = [doc.metadata['page'] for doc in res['source_documents']]
-	print(f"Answear refering the pages: {pages}")
+	print(f"Answear refering the following data.")
+	doc: Document
+	for doc in res['source_documents']:
+		print(f"[Page {doc.metadata.get('page')}] {doc.page_content}")
 	print("A:", res["result"])
